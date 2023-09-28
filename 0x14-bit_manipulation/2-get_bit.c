6ca9c2cc8539aa3ac1da2a17dcc4c086318979d4
Phve <muriujames58@gmail.com>
2023-09-28 14:15:32 -0700

    again

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * set_bit - Sets a bit at a given index to 1.
- * @n: Pointer to the number to modify.
- * @index: The index of the bit to set to 1 (0-based).
+ * get_bit - returs a vbalue of a bit at a decimal number
+ * @n: number to find
+ * @index: index of the current bit
  *
- * Return: 1 if the operation was successful, -1 if it failed.
+ * Return: value bit
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	/* Check if the provided index is within valid range (0 to 63). */
+	int bit_val;
+
 	if (index > 63)
 		return (-1);
 
-	/* Set the bit at the specified index to 1 using a bitwise OR operation. */
-	*n = ((1UL << index) | *n);
+	bit_val = (n >> index) & 1;
 
-	return (1); /* Return 1 to indicate success. */
+	return (bit_val);
 }
+
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a specified bit to 0.
- * @n: Pointer to the number to modify.
- * @index: The index of the bit to clear (0-based).
+ * set_bit - Sets a bit at a given index to 1.
+ * @n: pointer for the changed number
+ * @index: index of the bit to set to 1
  *
- * Return: 1 if the operation was successful, -1 if it failed.
+ * Return: 1 for success, -1 for failure
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* Check if the provided index is within a valid range (0 to 63). */
 	if (index > 63)
 		return (-1);
 
-	/* Use a bitwise AND complemented bit mask to clear the bit. */
-	*n = (~(1UL << index) & *n);
-
-	return (1); /* Return 1 to indicate success. */
+	*n = ((1UL << index) | *n);
+	return (1);
 }
